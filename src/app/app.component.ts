137ea8e1ddd08ecaea7d2aceae5103245ae0cc50
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-03-06 10:17:07 -0300

    feat(product-search): add search to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from '@ecommerce/layout';
+import { ProductSearchComponent } from '@ecommerce/product-search';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    LayoutModule,
+    ProductSearchComponent,
+  ],
   selector: 'ecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',

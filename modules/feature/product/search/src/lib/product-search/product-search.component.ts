c64eb9d39b81e892d8b0cb4448075c5d4a8a1f1d
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-03-06 21:21:55 -0300

    feat(product-search): products filter

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,10 +1,19 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { mockProducts } from '@ecommerce/product-data-access';
+import { ProductSearchService } from '@ecommerce/product-data-access';
+import { Product } from 'modules/data-access/product/src/lib/models/product.model';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'ecommerce-product-search',
@@ -16,11 +25,27 @@ import { mockProducts } from '@ecommerce/product-data-access';
     MatAutocompleteModule,
     ReactiveFormsModule,
     AsyncPipe,
+    HttpClientModule,
   ],
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
+  providers: [ProductSearchService],
 })
-export class ProductSearchComponent {
+export class ProductSearchComponent implements OnInit {
   control = new FormControl();
-  products = mockProducts;
+  products$!: Observable<Product[]>;
+
+  constructor(private productSearchService: ProductSearchService) {}
+
+  ngOnInit(): void {
+    // eslint-disable-next-line no-console
+    this.products$ = this.control.valueChanges.pipe(
+      // operadores
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter((text) => text.length > 1),
+      // map((text) => 'TEXTO TRANSFORMADO '+text),
+      switchMap((text) => this.productSearchService.searchByName(text))
+    );
+  }
 }

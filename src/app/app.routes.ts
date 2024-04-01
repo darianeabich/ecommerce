ffe462bd445743c1dbabe9cbfe98b7309da84efe
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-04-01 17:07:57 -0300

    chore: customizing styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    // Com loadChildren ou loadComponent
+    // loadComponent: () => import('@ecommerce/home').then((c) => c.HomeComponent),
+    loadChildren: () => import('@ecommerce/home').then((c) => c.homeRoutes),
+  },
+];

07befb3bd499ce4c5ec912dc830d9a764ecd34d3
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-04-04 20:38:11 -0300

    feat(home): use recommended products service

--- modules/feature/home/src/lib/home/home.component.spec.ts
+++ modules/feature/home/src/lib/home/home.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  RecommendedProductsService,
+  mockProducts,
+} from '@ecommerce/product-data-access';
+import { of } from 'rxjs';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,6 +13,14 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [
+        {
+          provide: RecommendedProductsService,
+          useValue: {
+            getProducts: () => of(mockProducts),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);

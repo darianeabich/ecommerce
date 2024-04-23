4d6e3c5031491eb131219ddb28e059e4c150f9b0
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-04-22 23:08:27 -0300

    fix: test in home component

--- modules/feature/home/src/lib/home/home.component.spec.ts
+++ modules/feature/home/src/lib/home/home.component.spec.ts
@@ -31,4 +31,10 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render product cards correctly', () => {
+    const cards: HTMLElement[] =
+      fixture.nativeElement.querySelectorAll('mat-card');
+    expect(cards.length).toBe(mockProducts.length);
+  });
 });

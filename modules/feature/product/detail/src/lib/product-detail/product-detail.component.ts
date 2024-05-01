6c66b2ef07d3d5137b00bb998c65ea28feeb93d6
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-05-01 00:17:16 -0300

    feat(product-detail): add product detail info

--- modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ProductSearchService } from '@ecommerce/product-data-access';
+import { ProductCardComponent } from '@ecommerce/product-ui';
 import { switchMap } from 'rxjs';
+import { QuantityDescriptionPipe } from '../pipes/quantity-description/quantity-description.pipe';
 import { getParamsId } from './get-params';
 
 @Component({
   selector: 'ecommerce-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent, QuantityDescriptionPipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
 })
@@ -15,7 +17,7 @@ export class ProductDetailComponent {
   // @Input() id!: string;
   productSearchService = inject(ProductSearchService);
 
-  products$ = getParamsId().pipe(
+  product$ = getParamsId().pipe(
     switchMap((id) => this.productSearchService.getById(id))
   );
 }

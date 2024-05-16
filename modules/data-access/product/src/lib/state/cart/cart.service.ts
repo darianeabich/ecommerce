0838f9be89f3d72b0ee0e14137f219a47a61a576
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-05-16 20:01:22 -0300

    (product-data-access): add cart state

---
+++ modules/data-access/product/src/lib/state/cart/cart.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, computed, signal } from '@angular/core';
+import { Product } from '../../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  // forma com OBSERVABLES
+  // private cartSubject$ = new BehaviorSubject<Product[]>([]);
+  // cart$ = this.cartSubject$.asObservable();
+  // quantity$ = this.cart$.pipe(map((products) => products.length));
+
+  // /**
+  //  * @description Add products to cart
+  //  * @param product
+  //  */
+  // addToCart(product: Product) {
+  //   const currentCart = this.cartSubject$.getValue();
+  //   this.cartSubject$.next([...currentCart, product]);
+  // }
+
+  // forma com SIGNALS
+  private cartSignal = signal<Product[]>([]);
+  cart = this.cartSignal.asReadonly();
+  quantity = computed(() => this.cart().length);
+
+  addToCart(product: Product): void {
+    this.cartSignal.update((products) => [...products, product]);
+  }
+}

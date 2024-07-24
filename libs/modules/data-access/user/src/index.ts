aa6123d6fd25334117ec17af2e5dc0454cecd7ca
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-07-24 18:37:43 -0300

    feat(user-data-access): add mock, model and service

---
+++ libs/modules/data-access/user/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/mocks/user.mock';
+export * from './lib/models/user.model';
+export * from './lib/user-list/user-list.service';
---
+++ libs/modules/data-access/user/src/lib/user-list/user-list.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserListService {
+  readonly apiUrl = 'https://65009f9718c34dee0cd53786.mockapi.io/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+}

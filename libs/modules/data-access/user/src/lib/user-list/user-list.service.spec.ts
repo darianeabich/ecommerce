860ead7b86097b06b66a02eb8809a684a65fa47b
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-08-04 19:39:46 -0300

    feat(user list service): add test

--- libs/modules/data-access/user/src/lib/user-list/user-list.service.spec.ts
+++ libs/modules/data-access/user/src/lib/user-list/user-list.service.spec.ts
@@ -1,16 +1,40 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { User } from '../models/user.model';
 import { UserListService } from './user-list.service';
 
 describe('UserListService', () => {
   let service: UserListService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(UserListService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return users correctly', () => {
+    // Arrange
+    const url = `${service.apiUrl}/users`;
+    let result: User[] = [];
+
+    // Act
+    service.getUsers().subscribe((users) => (result = users));
+
+    // Assert
+    const request = httpMock.expectOne(url);
+    request.flush([]);
+    expect(request.request.method).toBe('GET');
+    expect(result).toEqual([]);
   });
 });
